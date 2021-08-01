fd3a7b5dc6fc3c82de21c9c3b5201a392dc670dc
Ghassen <ghassen.boughzala@esprit.tn>
2021-08-01 21:04:00 +0100

    Auth UI Flatter

---
+++ lib/screens/login.dart
@@ -0,0 +1,195 @@
+import 'package:creditapp/screens/signup.dart';
+import 'package:flutter/material.dart';
+import 'package:creditapp/services/authservice.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+
+class LoginPage extends StatelessWidget {
+  var name, password, token;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      resizeToAvoidBottomInset: false,
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        elevation: 0,
+        brightness: Brightness.light,
+        backgroundColor: Colors.white,
+        leading: IconButton(
+          onPressed: () {
+            Navigator.pop(context);
+          },
+          icon: Icon(Icons.arrow_back_ios,
+          size: 20,
+          color: Colors.black,),
+
+
+        ),
+      ),
+      body: Container(
+        height: MediaQuery.of(context).size.height,
+        width: double.infinity,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: <Widget>[
+            Expanded(child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: <Widget>[
+                Column(
+                  children: <Widget>[
+                    Text("Login",
+                    style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),),
+                    SizedBox(height: 20,),
+                    Text("Login to your account",
+                    style: TextStyle(
+                      fontSize: 15,
+                    color:Colors.grey[700]),)
+                  ],
+                ),
+                Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 40),
+                  child: Column(
+                    children: <Widget>[
+                      TextField(
+                        decoration: InputDecoration(
+                          labelText: 'Name'
+                        ),
+                        onChanged: (val){ name = val;},
+                      ),
+                      TextField(
+                        obscureText: true,
+                        decoration: InputDecoration(labelText: 'Password'),
+                        onChanged: (val) { password = val; },
+                      )
+                    ],
+                  ),
+                ),
+                  Padding(padding:
+                  EdgeInsets.symmetric(horizontal: 40),
+                  child: Container(
+                      padding: EdgeInsets.only(top: 3, left: 3),
+                      decoration:
+                        BoxDecoration(
+                          borderRadius: BorderRadius.circular(50),
+                          border: Border(
+                            bottom: BorderSide(color: Colors.black),
+                            top: BorderSide(color: Colors.black),
+                            left: BorderSide(color: Colors.black),
+                            right: BorderSide(color: Colors.black),
+
+                          )
+
+
+
+                        ),
+                      child: MaterialButton(
+                        minWidth: double.infinity,
+                        height: 60,
+                        onPressed: (){
+                          AuthService().login(name, password).then((val){
+                            token = val.data['token'];
+                            Fluttertoast.showToast(
+                              msg:'Successfully Authenticated ',
+                              toastLength: Toast.LENGTH_SHORT,
+                              gravity: ToastGravity.BOTTOM,
+                              timeInSecForIosWeb: 2,
+                              backgroundColor: Colors.green,
+                              textColor: Colors.white,
+                              fontSize: 16.0
+                            );
+                          });
+                        } ,
+                        color: Color(0xff3868B2),
+                        elevation: 0,
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(50),
+
+                        ),
+                        child: Text(
+                          "Login", style: TextStyle(
+                          fontWeight: FontWeight.w600,
+                          fontSize: 18,
+                          color: Colors.white,
+
+                        ),
+                        ),
+
+                      ),
+                    ),
+                  ),
+
+
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Text("Don't have an account?"),
+                      TextButton(
+                        child:  Text(" Sign up",                      
+                        style: TextStyle(
+                          fontWeight: FontWeight.w600,
+                          fontSize: 18,),
+                        ),
+                        onPressed: (){
+                        Navigator.push(context, MaterialPageRoute(builder: (context)=> SignupPage()));},
+                    )
+                   
+                  ],
+                ),
+
+                Container(
+                  padding: EdgeInsets.only(top: 100),
+                  height: 200,
+                  decoration: BoxDecoration(
+                   
+
+                  ),
+                )
+
+              ],
+            ))
+          ],
+        ),
+      ),
+    );
+  }
+
+}
+
+
+// we will be creating a widget for text field
+Widget inputFile({label, obscureText = false})
+{
+  return Column(
+    crossAxisAlignment: CrossAxisAlignment.start,
+    children: <Widget>[
+      Text(
+        label,
+        style: TextStyle(
+          fontSize: 15,
+          fontWeight: FontWeight.w400,
+          color:Colors.black87
+        ),
+
+      ),
+      SizedBox(
+        height: 5,
+      ),
+      TextField(
+        obscureText: obscureText,
+        decoration: InputDecoration(
+          contentPadding: EdgeInsets.symmetric(vertical: 0,
+          horizontal: 10),
+          enabledBorder: OutlineInputBorder(
+            borderSide: BorderSide(
+              color: Colors.grey
+            ),
+
+          ),
+          border: OutlineInputBorder(
+            borderSide: BorderSide(color: Colors.grey)
+          )
+        ),
+      ),
+      SizedBox(height: 10,)
+    ],
+  );
+}
--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -1,6 +1,7 @@
 import 'package:creditapp/services/authservice.dart';
-import 'package:flutter/material.dart';
 import 'package:fluttertoast/fluttertoast.dart';
+
+import 'package:flutter/material.dart';
 
 class LoginScreen extends StatefulWidget {
   const LoginScreen({ Key? key }) : super(key: key);
